6af8cb0331663398c79846ce8494522cc5ba52b8
Quang Son <quangson.laptrinh@gmail.com>
2022-05-11 06:35:24 +0700

    update

--- Laravel Pro/blog/database/migrations/2022_05_10_152459_add_gender_to_pays_table.php
+++ Laravel Pro/blog/database/migrations/2022_05_10_152459_add_gender_to_pays_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+class AddGenderToPaysTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
-            $table->string('content');
-            $table->string('user_id');
-            $table->timestamps();
+        Schema::table('pays', function (Blueprint $table) {
+            //
+            $table->enum('gender',['male','female'])->nullablr();
         });
     }
 
@@ -29,6 +26,9 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::table('pays', function (Blueprint $table) {
+            //
+            $table->dropColumn('gender');
+        });
     }
 }
