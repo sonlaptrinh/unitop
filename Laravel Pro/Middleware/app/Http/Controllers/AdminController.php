a30983ddeb0c0cd4896b2067b67c4229ea424822
QuangSon <quangson.laptrinh@gmail.com>
2022-05-10 16:37:22 +0700

    update

---
+++ Laravel Pro/Middleware/app/Http/Controllers/AdminController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use function Illuminate\Support\Facades\Storage;
+
+class AdminController extends Controller
+{
+    //
+
+}
